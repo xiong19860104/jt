e1e057890c8c44d57ea35d7c280fe9901672b863
xiongwenting <499626079@qq.com>
2019-10-16 18:24:08 +0800

    xiong

--- cgb-sboot-01/src/main/java/com/cn/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.cn.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-@Mapper
-public interface UserDao {
-	public int deleteObjects(@Param("ids")Integer []ids);
-
-}
